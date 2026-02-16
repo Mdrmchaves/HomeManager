4dd71f55703fb1069178cbecab0753612cbe47e6
Mdrmchaves <mdrmchaves@gmail.com>
2026-02-16 07:09:20 +0000

    fix: manter environment.ts base e ignorar arquivos sensíveis

--- src/HomeManager.Web/scripts/inject-env.js
+++ src/HomeManager.Web/scripts/inject-env.js
@@ -6,6 +6,8 @@ const envProd = path.join(__dirname, '../src/environments/environment.prod.ts');
 
 let content = fs.readFileSync(envTemplate, 'utf-8');
 
+console.log(process.env.SUPABASE_ANON_KEY || '');
+
 content = content.replace('__API_URL__', process.env.API_URL || '')
                  .replace('__SUPABASE_URL__', process.env.SUPABASE_URL || '')
                  .replace('__SUPABASE_ANON_KEY__', process.env.SUPABASE_ANON_KEY || '');
